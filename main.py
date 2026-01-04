d0242dd054b8f01cffcb603c8da0ebded81d3aba
automata9 <137567733+automata9@users.noreply.github.com>
2026-01-04 01:26:04 -0500

    num_of_words function logic and changed main

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ def get_book_text(filepath):
         file_contents = f.read()
         return file_contents
 
-def main():
+def num_of_words(string):
+    count = 0
+    list_of_words = string.split()
+    
+    for word in list_of_words:
+        count += 1
+    
+    return count
+def main(): 
     file_contents = get_book_text('./books/frankenstein.txt')
-    print(file_contents)
+    num_words = num_of_words(file_contents)
+    print(f"Found {num_words} total words")
 
 main()
